5858a257d0d3f42d277fc32a423e80a1ae205723
Mike Moore <mike@blowmage.com>
2015-04-23 15:42:18 -0600

    Update the sample app to use gcloud-ruby
    The calls to the Storage API are easier, so this app does more now.

--- sample.rb
+++ sample.rb
@@ -1,109 +1,101 @@
-require 'google/api_client'
-require 'highline/import'
-require './oauth_util'
+require "gcloud/storage"
+require "highline/import"
 
 # Constants for use as request parameters.
-DEFAULT_PROJECT = 'YOUR_PROJECT_ID'
-DEFAULT_BUCKET = 'YOUR_DEFAULT_BUCKET'
-DEFAULT_OBJECT = 'YOUR_DEFAULT_OBJECT'
-DEFAULT_ENTITY = 'YOUR_DEFAULT_ENTITY'
-API_VERSION = 'v1'
+PROJECT = ENV["PUBSUB_PROJECT"] || "YOUR_PROJECT_ID"
+KEYFILE = ENV["PUBSUB_KEYFILE"] || "./client_secrets.json"
 
-# Creating a new API client and loading the Google Cloud Storage API.
-client = Google::APIClient.new
-storage = client.discovered_api('storage', API_VERSION)
+storage = Gcloud.storage PROJECT, KEYFILE
 
-# OAuth authentication.
-auth_util = CommandLineOAuthHelper.new(
-  'https://www.googleapis.com/auth/devstorage.full_control')
-client.authorization = auth_util.authorize()
+file_menu = lambda do |file|
+  loop do
+    choose do |menu|
+      menu.header = "You are viewing file #{file.name}"
 
-# Linking each input selection to an API request.
-api_request_selection_map = {
-  '1' => storage.buckets.list,
-  '2' => storage.objects.list,
-  '3' => storage.bucket_access_controls.list,
-  '4' => storage.object_access_controls.list,
-  '5' => storage.buckets.get,
-  '6' => storage.objects.get,
-  '7' => storage.bucket_access_controls.get,
-  '8' => storage.object_access_controls.get
-}
+      menu.choice("List Access Control Lists for #{file.name}") do
+        say "ACLs for #{file.name} are:"
+        say "* Owners: #{file.acl.owners.join(", ")}"
+        say "* Writers: #{file.acl.writers.join(", ")}"
+        say "* Readers: #{file.acl.readers.join(", ")}"
+      end
 
-# Linking each API request to appropriate request parameters.
-api_request_parameter_map = {
-  storage.buckets.list => {
-    'project' => DEFAULT_PROJECT
-  },
-  storage.objects.list => {
-    'bucket' => DEFAULT_BUCKET
-  },
-  storage.bucket_access_controls.list => {
-    'bucket' => DEFAULT_BUCKET
-  },
-  storage.object_access_controls.list => {
-    'bucket' => DEFAULT_BUCKET,
-    'object' => DEFAULT_OBJECT
-  },
-  storage.buckets.get => {
-    'bucket' => DEFAULT_BUCKET
-  },
-  storage.objects.get => {
-    'bucket' => DEFAULT_BUCKET,
-    'object' => DEFAULT_OBJECT
-  },
-  storage.bucket_access_controls.get => {
-    'bucket' => DEFAULT_BUCKET,
-    'entity' => DEFAULT_ENTITY
-  },
-  storage.object_access_controls.get => {
-    'bucket' => DEFAULT_BUCKET,
-    'object' => DEFAULT_OBJECT,
-    'entity' => DEFAULT_ENTITY
-  }
-}
+      menu.choice("Display file details") do
+        say "Details for #{file.name}:"
+        say "* name: #{file.name}"
+        say "* size: #{file.size}"
+        say "* url: #{file.url}"
+        say "* etag: #{file.etag}"
+        say "* md5: #{file.md5}"
+        say "* crc32c: #{file.crc32c}"
+      end
 
-# Linking each API request to an appropriate request body.
-api_request_body_map = {
-}
+      menu.choices("Back to bucket") { return }
+    end
+  end
+end
 
-# REPL style interface for making API requests.
-while true
-  print "[1] List Buckets \n"
-  print "[2] List Objects \n"
-  print "[3] List Bucket Access Control Lists \n"
-  print "[4] List Object Access Control Lists \n"
-  print "[5] Get Bucket \n"
-  print "[6] Get Object \n"
-  print "[7] Get Bucket Access Control List \n"
-  print "[8] Get Object Access Control List \n"
-  print "Press any other key to exit \n"
-  print "\n"
-  api_selection = ask "Please select an API request from the above list. \n"
-  case api_selection
-  when '1'..'8'
-    api_request = api_request_selection_map[api_selection]
+bucket_menu = lambda do |bucket|
+  loop do
+    choose do |menu|
+      menu.header = "You are viewing bucket #{bucket.name}"
 
-    # Executing the selected API request, passing along an appropriate set of
-    # request parameters and a request body.
-    result = nil
-    if api_request_body_map[api_request]
-      result = client.execute(
-        :api_method => api_request,
-        :parameters => api_request_parameter_map[api_request],
-        :body_object => api_request_body_map[api_request]
-      )
-    else
-      result = client.execute(
-        :api_method => api_request,
-        :parameters => api_request_parameter_map[api_request]
-      )
+      menu.choice("List Access Control Lists for #{bucket.name}") do
+        say "ACLs for #{bucket.name} are:"
+        say "* Owners: #{bucket.acl.owners.join(", ")}"
+        say "* Writers: #{bucket.acl.writers.join(", ")}"
+        say "* Readers: #{bucket.acl.readers.join(", ")}"
+      end
+
+      menu.choice("List Default Access Control Lists for #{bucket.name}") do
+        say "Default ACLs for #{bucket.name} are:"
+        say "* Owners: #{bucket.default_acl.owners.join(", ")}"
+        say "* Writers: #{bucket.default_acl.writers.join(", ")}"
+        say "* Readers: #{bucket.default_acl.readers.join(", ")}"
+      end
+
+      menu.choice("List all files for #{bucket.name}") do
+        say "Files for #{bucket.name} are:"
+        bucket.files.each { |file| say file.name }
+      end
+
+      menu.choice("Inspect a file") do
+        file_name = ask "Which file do you want to inspect?"
+        file = bucket.find_file file_name
+        if file.nil?
+          say "Sorry, but #{file_name} does not exist in #{bucket.name}."
+        else
+          file_menu.call file
+        end
+      end
+
+      menu.choices("Back to project") { return }
+    end
+  end
+end
+
+loop do
+  choose do |menu|
+    menu.header = "You are viewing the storage for project #{storage.project}"
+
+    menu.choice("List all buckets for #{storage.project}") do
+      say "Buckets for #{storage.project} are:"
+      storage.buckets.each { |bucket| say "* #{bucket.name}" }
     end
 
-    print result.body
-  else
-    print 'Not a valid selection, please run program again and select a valid' +
-    " API request. \n"
-    exit
+    menu.choice("Inspect a bucket") do
+      bucket_name = ask "Which bucket do you want to inspect?"
+      begin
+        bucket = storage.find_bucket bucket_name
+        if bucket.nil?
+          say "Sorry, but #{bucket_name} does not exist."
+        else
+          bucket_menu.call bucket
+        end
+      rescue Gcloud::Storage::Error => e
+        say "Unable to load bucket #{bucket_name}. Recieved the error: #{e}"
+      end
+    end
+
+    menu.choices("Exit") { exit }
   end
 end
